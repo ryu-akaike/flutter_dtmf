2f7a249ed207e6d2a6fdb27ae96287e369f75d62
Ryu Akaike <r.akaike@nsys.co.jp>
2026-01-08 14:38:10 +0900

    Changed to use AVAudioPlayer to avoid deadlocks around Audio Session

--- ios/Classes/SwiftDtmfPlugin.swift
+++ ios/Classes/SwiftDtmfPlugin.swift
@@ -3,36 +3,16 @@ import UIKit
 import AVFoundation
 import CallKit
 
-public class SwiftDtmfPlugin: NSObject, FlutterPlugin {
+public class SwiftDtmfPlugin: NSObject, FlutterPlugin, AVAudioPlayerDelegate {
     
-    var _engine: AVAudioEngine
-    var _player:AVAudioPlayerNode
-    var _mixer: AVAudioMixerNode
-    var _playCount = 0
-    
-    public override init() {
-        _engine = AVAudioEngine();
-        _player = AVAudioPlayerNode()
-        _mixer = _engine.mainMixerNode;
-        
-        super.init()
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(handleRouteChange), name: AVAudioSession.routeChangeNotification, object: nil)
-    }
+    private var _player: AVAudioPlayer?
+    private var _session = AVAudioSession.sharedInstance()
+    private var _playing = false
     
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "flutter_dtmf", binaryMessenger: registrar.messenger())
         let instance = SwiftDtmfPlugin()
         registrar.addMethodCallDelegate(instance, channel: channel)
-    }
-    
-    @objc func handleRouteChange(notification: Notification){
-      _player.stop()
-      if(_engine.isRunning){_engine.disconnectNodeOutput(_player)}
-      _engine.stop()
-      _engine = AVAudioEngine();
-      _player = AVAudioPlayerNode()
-      _mixer = _engine.mainMixerNode;
     }
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
@@ -48,72 +28,109 @@ public class SwiftDtmfPlugin: NSObject, FlutterPlugin {
         
     }
     
+    private func uint16le(_ v: UInt16) -> Data {
+        var x = v.littleEndian
+        return Data(bytes: &x, count: MemoryLayout<UInt16>.size)
+    }
+    private func uint32le(_ v: UInt32) -> Data {
+        var x = v.littleEndian
+        return Data(bytes: &x, count: MemoryLayout<UInt32>.size)
+    }
+    
+    func makeDtmfData(digits: String, samplingRate: Double, durationMs: Int) -> Data? {
+        
+        guard let tones = DTMF.tonesForString(digits) else {return nil}
+        
+        var pcm16 = [Int16]()
+        
+        for tone in tones {
+            let samples = DTMF.generateDTMF(tone, markSpace: MarkSpaceType(Float(durationMs), Float(durationMs)), sampleRate: Float(samplingRate))
+            for sample in samples {
+                let v = max(-1.0, min(1.0, sample))
+                pcm16.append(Int16(v * Float(Int16.max)))
+            }
+        }
+        
+        let numChannels: UInt16 = 1
+        let bitsPerSample: UInt16 = 16
+        let byteRate = UInt32(samplingRate) * UInt32(numChannels) * UInt32(bitsPerSample / 8)
+        let blockAlign = UInt16(numChannels) * (bitsPerSample / 8)
+        let dataSize = UInt32(pcm16.count * MemoryLayout<Int16>.size)
+
+        var data = Data()
+        
+        
+        // RIFF header
+        data.append("RIFF".data(using: .ascii)!)
+        data.append(uint32le(36 + dataSize))
+        data.append("WAVE".data(using: .ascii)!)
+
+        // fmt chunk
+        data.append("fmt ".data(using: .ascii)!)
+        data.append(uint32le(16)) // PCM fmt chunk size
+        data.append(uint16le(1))  // audio format 1=PCM
+        data.append(uint16le(numChannels))
+        data.append(uint32le(UInt32(samplingRate)))
+        data.append(uint32le(byteRate))
+        data.append(uint16le(blockAlign))
+        data.append(uint16le(bitsPerSample))
+
+        // data chunk
+        data.append("data".data(using: .ascii)!)
+        data.append(uint32le(dataSize))
+        
+        // PCM payload
+        pcm16.withUnsafeBufferPointer { bp in
+            data.append(Data(buffer: bp))
+        }
+        
+        return data
+    }
+    
     func playTone(digits: String, volume: Double?, samplingRate: Double, durationMs: Int, flutterResult: @escaping FlutterResult)
     {
         
-        let _sampleRate = Float(samplingRate)
-        
-        if let tones = DTMF.tonesForString(digits) {
-            let audioFormat = AVAudioFormat(commonFormat: .pcmFormatFloat32, sampleRate: Double(_sampleRate), channels: 2, interleaved: false)!
-            
-            // fill up the buffer with some samples
-            var allSamples = [Float]()
-            for tone in tones {
-                let samples = DTMF.generateDTMF(tone, markSpace: MarkSpaceType(Float(durationMs), Float(durationMs)), sampleRate: _sampleRate)
-                allSamples.append(contentsOf: samples)
+        do {
+            try _session.setCategory(.playback, options: [.mixWithOthers])
+            if !_session.isOtherAudioPlaying {
+                try _session.setActive(true)
             }
-            
-            let frameCount = AVAudioFrameCount(allSamples.count)
-            let buffer = AVAudioPCMBuffer(pcmFormat: audioFormat, frameCapacity: frameCount)!
-            
-            buffer.frameLength = frameCount
-            let channelMemory = buffer.floatChannelData!
-            for channelIndex in 0 ..< Int(audioFormat.channelCount) {
-                let frameMemory = channelMemory[channelIndex]
-                memcpy(frameMemory, allSamples, Int(frameCount) * MemoryLayout<Float>.size)
-            }
-            
-            _engine.attach(_player)
-            _engine.connect(_player, to:_mixer, format:audioFormat)
-            
-            do {
-                try _engine.start()
-            } catch let error as NSError {
-                flutterResult(false)
-                print("Engine start failed - \(error)")
-                return
-            }
-            
-            _player.scheduleBuffer(buffer, at:nil,completionCallbackType: .dataPlayedBack){  [weak self] _ in
-                Task{ @MainActor in
-                    self?.stopTone()
-                }
-            }
-            if (volume != nil) {
-                _player.volume = Float(volume!)
-            }
-            _player.play()
-            _playCount += 1
-            flutterResult(true)
+        } catch {
+            print("AudioSession setCategory/setActive failed: \(error)")
         }
-    }
-    
-    private func stopTone(){
-        _playCount -= 1
-        if _playCount > 0 {
+
+        guard let wavData = makeDtmfData(digits: digits, samplingRate: samplingRate, durationMs: durationMs) else {
+            flutterResult(false)
             return
         }
         
-        _player.stop()
-        if _engine.isRunning {
-            _engine.stop()
-        }
         do{
-            let session = AVAudioSession.sharedInstance()
-            try session.setCategory(.ambient, mode: .default, options: [])
-            try session.setActive(false, options: [.notifyOthersOnDeactivation])
+            let p = try AVAudioPlayer(data: wavData)
+            p.volume = Float(volume ?? 1.0)
+            p.delegate = self
+            p.prepareToPlay()
+            DispatchQueue.main.async{
+                self._player?.stop()
+                self._player = p
+                self._playing = p.play()
+                flutterResult(self._playing)
+            }
         }catch{
-            print("Deactivate failed - \(error)")
+            print("AVAudioPlayer init failed: \(error)")
+            flutterResult(false)
+        }
+        
+    }
+    
+    public func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        
+        _playing = false
+        
+        do{
+            try _session.setCategory(.ambient, mode: .default, options: [])
+            try _session.setActive(false, options: [.notifyOthersOnDeactivation])
+        }catch{
+            print("AudioSession setCategory/setActive failed: \(error)")
         }
     }
 }
